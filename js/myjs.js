0c0cb45922574608d8b2af79f4eea8d04eabdae7
seentheunseen <68526872+seentheunseen@users.noreply.github.com>
2020-07-21 14:30:45 +0800

    Update myjs.js

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,22 @@
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
+
+//remove inspect element
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
